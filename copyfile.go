3b98c017f0606ed98bc1b7d1cb7a7394f64e5fa6
zhaohao <1154283293@qq.com>
2020-03-25 04:47:32 -0400

    add copy tools

---
+++ copyfile.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"io"
+	"fmt"
+	"flag"
+	"path"
+)
+
+var (
+	src string 
+	dst string
+)
+
+func init() {
+	flag.StringVar(&src ,"srcfile" ,"src.txt" ,"add source file")
+	flag.StringVar(&dst ,"dstfile" ,"dst.txt" ,"add destation file")
+	flag.Parse()
+	fmt.Println(src ,dst)
+}
+
+func CopyFile(srcname ,dstname string)(written int64 ,err error){
+	srcfile,err := os.OpenFile(srcname ,os.O_RDONLY, 0666)
+	if err != nil {
+		fmt.Printf("源文件打开失败!\n")
+		return 
+	}
+	defer srcfile.Close()
+	
+	dstfile,err := os.OpenFile(dstname ,os.O_CREATE | os.O_TRUNC | os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Printf("目的文件打开失败!\n")
+		return 
+	}
+
+	defer dstfile.Close()
+	return io.Copy(dstfile, srcfile)
+}
+
+func main() {
+	if dst == "./" {
+		_ , filename := path.Split(src)
+		dst = "./" + filename
+	}
+	_ ,err := CopyFile(src ,dst)
+	if err != nil {
+		fmt.Printf("文件拷贝失败：\n",err)
+		return
+	}
+	fmt.Printf("文件拷贝完成!\n") 
+}
